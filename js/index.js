026ee74cef63d2ab2901465721ed1bcaa1034e5f
Sifei Liu <sifeil@nvidia.com>
2026-06-04 15:59:07 -0700

    Polish new teasers, Cosmos 3 entry, and author list UX
    Replace new papers teasers with cleaner assets (GR3D, LoHoManip 12s loop, CGSPN generation grid, OmniVinci chart, Cosmos 3 clip). Cosmos 3: remove sole-author list, reword to first person, add Tech Report PDF link via new pdf template field. Widen image column is-3 to is-4. Collapse long author lists to 2 lines with a Show all authors toggle.

--- js/index.js
+++ js/index.js
@@ -8,4 +8,32 @@ $(document).ready(function() {
         $(this).find('video').css('display', 'none');
         $(this).find('img').css('display', 'inline-block');
     });
+
+    function setupAuthorToggles() {
+        $('.publication-authors').each(function() {
+            var container = this;
+            var list = container.querySelector('.authors-list');
+            var toggle = container.querySelector('.authors-toggle');
+            if (!list || !toggle) return;
+            var overflowing = list.scrollHeight - list.clientHeight > 2;
+            if (overflowing && !container.classList.contains('is-expanded')) {
+                toggle.hidden = false;
+            } else if (!container.classList.contains('is-expanded')) {
+                toggle.hidden = true;
+            }
+        });
+    }
+
+    $(document).on('click', '.authors-toggle', function() {
+        var container = $(this).closest('.publication-authors')[0];
+        var expanded = container.classList.toggle('is-expanded');
+        this.textContent = expanded ? 'Show less' : 'Show all authors';
+    });
+
+    setupAuthorToggles();
+    var resizeTimer;
+    $(window).on('resize', function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(setupAuthorToggles, 150);
+    });
 })
